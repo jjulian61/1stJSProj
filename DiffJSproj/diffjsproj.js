530ce8ad3521ab6f965236ac7a4eaee7080a7981
John <jjulian61@gmail.com>
2025-03-03 00:03:55 -0500

    adding search bar for year

--- DiffJSproj/diffjsproj.js
+++ DiffJSproj/diffjsproj.js
@@ -1,9 +1,12 @@
 const searchtitle = document.getElementById('search__title'); 
 const searchbar = document.getElementById('filtertitle');
+const searchyear = document.getElementById('search__year')
+const yearbar = document.getElementById('filteryear')
 let moviedata = [];
 
 //Fetch data from API
 async function main() {
+  const title = document.getElementById("filtertitle").value
     const movies = await fetch(`https://www.omdbapi.com/?apikey=6c0f2b1f&s=${title}`);
     const moviesdata = await movies.json();
     const userlitEl = document.querySelector(".user-list");
@@ -12,6 +15,8 @@ async function main() {
       const searchTerm = event.target.input;
       console.log("Input changed:", searchTerm);
       // Perform actions based on input change
+
+      
   });
 
   
@@ -39,6 +44,35 @@ async function main() {
 }
 
 main();
+
+async function second() {
+  const year = document.getElementById("filteryear").value
+    const movies1 = await fetch(`https://www.omdbapi.com/?apikey=6c0f2b1f&s=${year}`);
+    const moviesdata1 = await movies1.json();
+    const userlitEl1 = document.querySelector(".user-list");
+
+    searchyear.addEventListener ("input", (event) => {
+      const searchTerm1 = event.target.input;
+      console.log("Input changed:", searchTerm);
+      // Perform actions based on input change
+
+      
+  });
+  userlitEl1.innerHTML = moviesdata1.Search.map((user) =>
+    `<div class="user-card">
+              <div class="user-card__container">
+                <h3 id="search__title">${user.Title}</h4>
+                  <p id="search__year"><b>Year:</b> ${user.Year}</p>
+                  <p id="search__id"><b>IMDB ID:</b> ${user.imdbID}</p>
+                  <p id="search__type"><b>Type:</b> ${user.Type}</p>
+                  <img src="${user.Poster}" alt="">
+              </div>
+    </div>`)
+    .join("");
+
+}
+
+second();
 
 
 
