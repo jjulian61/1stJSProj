e2b4454a7d7c6e0eab80caaa7b80aee111a2e284
John <jjulian61@gmail.com>
2025-03-04 04:14:06 -0500

    final edits

--- DiffJSproj/diffjsproj.js
+++ DiffJSproj/diffjsproj.js
@@ -45,35 +45,6 @@ async function main() {
 
 main();
 
-async function second() {
-  const year = document.getElementById("filteryear").value
-    const movies1 = await fetch(`https://www.omdbapi.com/?apikey=6c0f2b1f&s=${year}`);
-    const moviesdata1 = await movies1.json();
-    const userlitEl1 = document.querySelector(".user-list");
-
-    searchyear.addEventListener ("input", (event) => {
-      const searchTerm1 = event.target.input;
-      console.log("Input changed:", searchTerm);
-      // Perform actions based on input change
-
-      
-  });
-  userlitEl1.innerHTML = moviesdata1.Search.map((user) =>
-    `<div class="user-card">
-              <div class="user-card__container">
-                <h3 id="search__title">${user.Title}</h4>
-                  <p id="search__year"><b>Year:</b> ${user.Year}</p>
-                  <p id="search__id"><b>IMDB ID:</b> ${user.imdbID}</p>
-                  <p id="search__type"><b>Type:</b> ${user.Type}</p>
-                  <img src="${user.Poster}" alt="">
-              </div>
-    </div>`)
-    .join("");
-
-}
-
-second();
-
 
 
 
